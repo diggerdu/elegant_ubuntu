d41471803f68818cf4e7b43724c1bb2ab6cfe848
diggerdu <diggerdu@hotmail.com>
2016-06-12 05:40:52 +0800

    elegant ubuntu

---
+++ README.md
@@ -0,0 +1,12 @@
+# elegant_ubuntu
+==== 
+installation
+-------
+* sudo sh elegant.sh
+* launch unity-tweak-tool, change settings
+
+preview
+-------
+![image](https://github.com/diggerdu/elegant_ubuntu/raw/master/screenshots/overview.jpg)
+![image](https://github.com/diggerdu/elegant_ubuntu/raw/master/screenshots/terminal.jpg)
+![image](https://github.com/diggerdu/elegant_ubuntu/raw/master/screenshots/icon.jpg)
---
+++ elegant.sh
@@ -0,0 +1,42 @@
+#install notepadqq
+sudo add-apt-repository ppa:notepadqq-team/notepadqq
+sudo apt-get update
+sudo apt-get install notepadqq
+
+#install tuning tool
+sudo apt-get install unity-tweak-tool
+
+#install icon
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install ultra-flat-icons
+
+#install theme
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install flatabulous-theme
+
+#install font
+FONT_DIR=/usr/share/fonts/truetype/custom/
+sudo mkdir -p $FONT_DIR
+sudo mv MONACO.TTF $FONT_DIR
+sudo mv LiHei.ttf $FONT_DIR
+sudo fc-cache -f -v
+
+#install cursor theme
+tar -xf Breeze.tgz 
+mv Breeze ~/.icons/
+gsettings set org.gnome.desktop.interface cursor-theme Breeze
+gsettings set org.gnome.desktop.interface cursor-size 24
+touch ~/.Xresources 
+echo "Xcursor.theme: Breeze" | cat >> ~/.Xresources
+echo "Xcursor.size: 24" | cat >> ~/.Xresources 
+
+#chang background
+mkdir ~/Pictures
+mkdir ~/Pictures/Wallpapers
+cp background.jpg ~/Pictures/Wallpapers/
+gsettings set org.gnome.desktop.background picture-uri "file://${HOME}/Pictures/Wallpapers/background.jpg"
+gsettings set com.canonical.unity-greeter background '${HOME}/Pictures/Wallpapers/background.jpg'
+
+
