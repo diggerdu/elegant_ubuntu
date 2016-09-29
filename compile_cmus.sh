970bd083143126689c6c9cb14fb40b35769864e2
diggerdu <diggerdu@hotmail.com>
2016-09-29 15:32:13 +0800

    cmus & vim8

---
+++ compile_cmus.sh
@@ -0,0 +1,20 @@
+sudo apt-get install  libncursesw5-dev
+sudo apt-get install  libasound2-dev
+sudo apt-get install  libao-dev
+sudo apt-get install  libsamplerate0-dev
+sudo apt-get install  libcdio-dev
+sudo apt-get install  libcdio-cdda-dev
+sudo apt-get install  libcddb2-dev
+sudo apt-get install  libcue-dev
+sudo apt-get install  libflac-dev
+sudo apt-get install  libvorbis-dev
+sudo apt-get install  libmpcdec-dev
+sudo apt-get install  libwavpack-dev
+sudo apt-get install  libmad0-dev
+sudo apt-get install  libavformat-dev libavcodec-dev
+sudo apt-get install  libfaad-dev
+sudo apt-get install  libmp4v2-dev
+sudo apt-get install  libmodplug-dev
+sudo apt-get install  libmikmod2-dev
+./configure CONFIG_FLAC=y CONFIG_MAD=y CONFIG_WAV=y CONFIG_MP4=y CONFIG_AAC=y CONFIG_CUE=y CONFIG_ALSA=y
+./configure CONFIG_FLAC=y CONFIG_MAD=y CONFIG_WAV=y CONFIG_MP4=y CONFIG_AAC=y CONFIG_CUE=y CONFIG_ALSA=y
